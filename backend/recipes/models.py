eefa70caaee7ca56ceeda443da34281d70ef5c67
Lyric J Chrislock <lyricchrislock@gmail.com>
2025-01-28 19:34:29 -0800

    Created the models needed for the recipe, and linked them. I logged into the Django Administration page, and can confirm that I am able to see the Recipes model.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+# I am adding extra documentation below the class and def __str__, I have pylint installed and it is very angry with me right now.
+# I also figured it would be good for you all to know what is going on.
+
+
+class Recipe(models.Model):
+    """This is a basic model for storing recipe traits such as the title, description, ingredients, instructions, and as well as the time stamp."""
+    title = models.CharField(
+        max_length=200)  # Here is where we will store our recipe name
+    # Here is where we will store our recipe description
+    description = models.TextField()
+    # Here is where we will store our ingredients list
+    ingredients = models.TextField()
+    instructions = models.TextField()  # Here is where we will store our instructions
+    # This is a time stamp which helps with figuring out of course as you guessed the time the model was created!
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns a string that represents the recipe (the title)."""
+        return self.title
+    
