ccb26f0b4981a65115424fff9134c81b1d521491
Alaina <alaina.porquis@gmail.com>
2025-02-27 21:09:34 -0800

    fixing merge conflict 2

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Recipe
 
+
 class RecipeSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False) #gives the user the ability to add a recipe and NOT have to include an image.
     user = serializers.StringRelatedField(read_only=True)  # shows the username
