76ef7afb9f4f6d9b6d9af35235f679c2f61f2d06
Lyric J Chrislock <lyricchrislock@gmail.com>
2025-02-27 17:12:40 -0800

    added url for user data flush

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -9,17 +9,21 @@ from .views import (
     update_recipe,
     delete_recipe,
     get_user_info,
+    export_user_data
 )
 
 urlpatterns = [
+    #User data dump url and api feed (links to the dashboard download button)
+    path("export-user-data/", export_user_data, name="export-user-data"),
+
     # User Authentication URLs
     path("register/", register_user, name="register"),
     path("login/", login_user, name="login"),
 
-    # User Information URL
+    # User Information URL (this api links to the dashboard )
     path("user-info/", get_user_info, name="user-info"),
 
-    # Recipe API URLs
+    # Recipe API URLs for adding deletion updating etc
     path("", get_recipes, name="get_recipes"),
     path("add/", add_recipe, name="add_recipe"),
     path("update/<int:recipe_id>/", update_recipe, name="update_recipe"),
