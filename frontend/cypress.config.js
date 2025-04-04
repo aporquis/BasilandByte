7d176d2db9d5e91f83d54a0f6ae8ffa565cfc7ac
Alaina <alaina.porquis@gmail.com>
2025-04-03 20:48:42 -0700

    Implemented Cypress Testing Dependencies

---
+++ frontend/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://basilandbyte.vercel.app/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  }
+});
