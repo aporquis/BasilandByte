c970f12957a0c38260ed208821aabdecb5b564a3
Alaina <alaina.porquis@gmail.com>
2025-04-09 22:23:40 -0700

    Added user testing for user deletion.

---
+++ frontend/cypress/e2e/user_deleteUser.cy.js
@@ -0,0 +1,35 @@
+/// <reference types = "cypress" />
+
+describe('Delete User', () => {
+    it('allows a user to delete their account', () => {
+        cy.visit('https://basilandbyte.vercel.app');
+
+        cy.contains('Login').click(); //find and click Login button
+
+        //fill out login form
+        cy.get('input').eq(0).type('Test');
+        cy.get('input').eq(1).type('ThisIsTE$T');
+
+        cy.get('button[type="submit"]').click(); // click login
+        cy.contains('Dashboard').click(); // Go to Dashboard
+        cy.contains('Delete my Account').click(); // click delete my account
+
+        //in window popup, confirm deletion
+        cy.on('window:confirm', (text) => {
+            expect(text).to.contains('Are you sure? Your account will be deactivated immediately');
+            return true; // Click OK
+          });
+
+        //redirects to login
+        cy.url().should('include', '/login');
+
+        //attempt relogging in
+        cy.contains('Login').click(); //find and click Login button
+
+        //fill out login form
+        cy.get('input').eq(0).type('Test');
+        cy.get('input').eq(1).type('ThisIsTE$T');
+
+        cy.contains('Login Failed!').should('exist');
+    });
+});
