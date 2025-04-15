799603b2ea115dcbcbb36e03f326473f50db7e9f
Lyric J Chrislock <lyricchrislock@gmail.com>
2025-04-14 20:00:04 -0700

    Register Pantry (userinventory) model in the admin page.

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient, FoodGroup, SavedItem, LoginEvent, UserDeletion
+from .models import Recipe, Ingredient, RecipeIngredient, FoodGroup, SavedItem, LoginEvent, UserDeletion, UserInventory
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ admin.site.register(FoodGroup)
 admin.site.register(SavedItem)
 admin.site.register(LoginEvent)
 admin.site.register(UserDeletion)
+admin.site.register(UserInventory)
